8718c9758a7852969f55004ec2deb37dee5583d0
wan <conut22@naver.com>
2025-04-17 21:54:19 +0900

    ✨feat: Implement router with dynamic data fetching and 404 page
    - Set up routing with React Router and dynamic data fetching based on URL parameters using `useParams`.
    - Created a 404 error page to handle unmatched routes.
    - Incorporated conditional logic to fetch and display data based on the provided parameters (`year`, `month`).

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Global, ThemeProvider } from '@emotion/react';
+import AppRouter from './router.jsx';
+import globalStyle from './theme/globalStyle';
+import theme from './theme/theme';
 
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Global styles={globalStyle} />
+      <AppRouter>
+        <App />
+      </AppRouter>
+    </ThemeProvider>
   </StrictMode>
 );
---
+++ src/pages/CalendarPage.jsx
@@ -0,0 +1,12 @@
+import { useOutletContext } from 'react-router-dom';
+
+function CalendarPage() {
+  const { data } = useOutletContext();
+  return (
+    <div>
+      달력 페이지입니다. 날짜는 {data.year}.{data.month}
+    </div>
+  );
+}
+
+export default CalendarPage;
