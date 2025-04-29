db374c3950437f69aa00b4689f8b1fa609260619
wan <conut22@naver.com>
2025-04-29 15:57:18 +0900

    ♻️ refactor: extract common layout for Home, Stats, Calendar
    - Create a common layout component for Home, Stats, and Calendar
    - Extend the common layout to create page-specific layouts
    - Apply the layouts by importing them into each page component
    - Improve maintainability by centralizing layout structure

---
+++ src/layouts/BaseContent.jsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+const BaseContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: -34px;
+`;
+
+export default BaseContent;
